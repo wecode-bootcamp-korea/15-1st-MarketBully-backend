3dc4c944a4a86abb177e2bf1cf820f9d995ef758
Jacob <jacob.juhyung.lee@gmail.com>
2020-12-19 02:13:40 +0900

    Modify: 누락 테이블 추가 - 누락되었던 product_descriptions, detailed_images 테이블 추가 - models.py 수정 후 views.py, urls.py 등 알맞게 수정

--- product/models.py
+++ product/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
-
 class Category(models.Model):
     name = models.CharField(max_length=45)
     
     class Meta:
         db_table = 'categories'
-
+        
 class Subcategory(models.Model):
     name     = models.CharField(max_length=45)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     
     class Meta:
         db_table = 'subcategories'
@@ -42,20 +41,36 @@ class PackingType(models.Model):
 
 class Product(models.Model):
     name            = models.CharField(max_length=100)
-    subtitle        = models.CharField(max_length=100)
+    subtitle        = models.CharField(max_length=500)
     price           = models.DecimalField(max_digits=10, decimal_places=2)
-    discount        = models.ForeignKey(Discount, on_delete=models.PROTECT, null=True)
+    discount        = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True)
     sales_unit      = models.DecimalField(max_digits=10, decimal_places=2)
     weight          = models.DecimalField(max_digits=10, decimal_places=2)
-    delivery_type   = models.ForeignKey(DeliveryType, on_delete=models.PROTECT, null=True)
-    origin          = models.ForeignKey(Origin, on_delete=models.PROTECT, null=True)
-    packing_type    = models.ForeignKey(PackingType, on_delete=models.PROTECT, null=True)
+    delivery_type   = models.ForeignKey(DeliveryType, on_delete=models.SET_NULL, null=True)
+    origin          = models.ForeignKey(Origin, on_delete=models.SET_NULL, null=True)
+    packing_type    = models.ForeignKey(PackingType, on_delete=models.SET_NULL, null=True)
     allergy         = models.CharField(max_length=500)
     expiration_date = models.DateField()
-    notice          = models.CharField(max_length=200, null=True)
-    is_soldout      = models.BooleanField(default=False)
+    notice          = models.CharField(max_length=500, null=True)
+    is_soldout      = models.BooleanField(default = False)
     image_url       = models.URLField(max_length=2000)
-    subcategory     = models.ForeignKey(Subcategory, on_delete=models.PROTECT)
+    subcategory     = models.ForeignKey(Subcategory, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'products'
+    
+class DetailedImage(models.Model):
+    description_image_url = models.URLField(max_length=2000)
+    product_image_url     = models.URLField(max_length=2000)
+    product               = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'detailed_images'
+
+class ProductDescription(models.Model):
+    content        = models.TextField()
+    detailed_image = models.ForeignKey(DetailedImage, on_delete=models.CASCADE)
+    product        = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'product_descriptions'
