d9c2326dc38fc9efe5314c176633b862ced287d8
Hyeonju <hjulee12@gmail.com>
2020-12-19 15:05:48 +0900

    [15기 이현주] 회원가입 리뷰 후 수정 및 주소 테이블과 연결

--- user/views.py
+++ user/views.py
@@ -8,22 +8,26 @@ from my_settings import SECRET_KEY
 
 # Create your views here.
 class SignupView(View): 
-    def validate_account(self, account):  
-        REGEX_ACCOUNT = '^[a-z0-9]{6,}$'
+    def validate_account(self, account): # 6자 이상의 영문 또는 영문과 숫자를 조합 
+        REGEX_ACCOUNT_1 = '^[a-z]{6,}$'                      # 영문 6자 이상
+        REGEX_ACCOUNT_2 = '^(?=.*[0-9])(?=.*[a-z]).{6,}$'    # 영문+숫자 6자 이상
 
-        if not re.match(REGEX_ACCOUNT, account):
+        if not re.match(REGEX_ACCOUNT_1, account) and not re.match(REGEX_ACCOUNT_2, account):
             return False
         return True
 
+    def validate_password(self, password): # 10자 이상, 영문/숫자/특수문자만 허용하며 2개 이상 조합
+        REGEX_PASSWORD_1 = '^(?=.*[0-9])(?=.*[a-zA-Z]).{10,}$'                       # 영문+숫자 10자 이상
+        REGEX_PASSWORD_2 = '^(?=.*[!@#$%^&*()_+])(?=.*[a-zA-Z]).{10,}$'              # 영문+특수문자 10자 이상
+        REGEX_PASSWORD_3 = '^(?=.*[0-9])(?=.*[!@#$%^&*()_+]).{10,}$'                 # 숫자+특수문자 10자 이상
+        REGEX_PASSWORD_4 = '^(?=.*[0-9])(?=.*[!@#$%^&*()_+])(?=.*[a-zA-Z]).{10,}$'   # 영문+숫자+특수문자 10자 이상
 
-    def validate_password(self, password):
-        REGEX_PASSWORD = '^[A-Za-z0-9@#$%^&+=]{10,}$'
-
-        if not re.match(REGEX_PASSWORD, password):
+        if not re.match(REGEX_PASSWORD_1, password) and not re.match(REGEX_PASSWORD_2, password) \
+            and not re.match(REGEX_PASSWORD_3, password) and not re.match(REGEX_PASSWORD_4, password):
             return False
         return True
 
-    def validate_email(self, email): 
+    def validate_email(self, email): # @, . 포함 
         REGEX_EMAIL = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 
         if not re.match(REGEX_EMAIL, email):
@@ -32,7 +36,7 @@ class SignupView(View):
     
 
     def validate_phone_number(self, phone_number):
-        REGEX_PHONE_NUMBER = '^\d{3}?[-]\d{4}?[-]\d{4}$'
+        REGEX_PHONE_NUMBER = '^\d{3}?[-]\d{3,4}?[-]\d{4}$'
         
         if not re.match(REGEX_PHONE_NUMBER, phone_number):
             return False
@@ -50,11 +54,12 @@ class SignupView(View):
 
         try:
 
-            if not self.validate_account(data['account']):
-                return JsonResponse({"message":"INVALID_ID"}, status=400)
 
             if User.objects.filter(account=data['account']).exists():
                 return JsonResponse({"message":"USER_EXIST"}, status=409)
+
+            if not self.validate_account(data['account']):
+                return JsonResponse({"message":"INVALID_ID"}, status=400)
 
             if not self.validate_password(data['password']):
                 return JsonResponse({"message": "INVALID_PW"}, status=400)
@@ -65,22 +70,33 @@ class SignupView(View):
             if not self.validate_phone_number(data['phone_number']):
                 return JsonResponse({"message":"INVALID_PHONE_NUMBER"}, status=400)
 
-            else:
-                User.objects.create(
-                    account             = data['account'],
-                    password            = hashed_pw,
-                    name                = data['name'],
-                    email               = data['email'],
-                    phone_number        = data['phone_number'],
-                    gender              = Gender(id=3),
-                    birth_date          = birth_date,
-                    recommender         = recommender,
-                    event_name          = event_name,
-                    grade               = Grade(id=1),
-                    terms_and_condition = TermsAndCondition(id=1),
-                    mileage             = 0
-                    )
-                return JsonResponse({"message":"SUCCESS"}, status=200)
+            user_model = User(
+                account             = data['account'],
+                password            = hashed_pw,
+                name                = data['name'],
+                email               = data['email'],
+                phone_number        = data['phone_number'],
+                gender_id           = data['gender_id'],
+                birth_date          = birth_date,
+                recommender         = recommender,
+                event_name          = event_name,
+                grade_id            = data['grade_id'],
+                terms_and_condition = TermsAndCondition(id=1),
+                mileage             = 0
+                )
+            
+            user_model.save()
+
+            Address(
+                name=data['name'],
+                user=User.objects.get(id=user_model.id),
+                is_active=False
+                ).save()
+            
+            return JsonResponse({"message":"SUCCESS"}, status=200)
 
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
+
+        
+
