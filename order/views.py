618de21b76061aa46ab0f35b27c7c32ea8291ae1
supersaloon <supersaloon@gmail.com>
2020-12-22 11:09:45 +0900

    Modify: PR #7에 대한 리뷰 반영하여 수정 - 컨벤션 맞지 않는 부분 수정 - post 메소드에 트랜젝션 적용 - patch 메소드의 키 delta를 받는 부분에서 에러메세지가 모호하던 부분 명확히 수정

--- order/views.py
+++ order/views.py
@@ -1,15 +1,17 @@
 import json
 from datetime import datetime
 
-from django.http      import JsonResponse
-from django.views     import View
+from django.http  import JsonResponse
+from django.views import View
+from django.db    import transaction
 
-from order.models   import Cart, Order
-from user.models      import Address
+from order.models import Cart, Order
+from user.models  import Address
 
 
 class CartView(View):
     # @login_required
+    @transaction.atomic
     def post(self, request):  # 장바구니에 상품 담기
         try:
             data = json.loads(request.body)
@@ -17,26 +19,26 @@ class CartView(View):
 
             # 사용자 장바구니 가져오기, 있으면 가져오고 없으면 생성하여 가져옴
             if not Order.objects.filter(user_id=user_id, status_id=1).exists():
-                order_cart = Order.objects.create(
+                new_order = Order.objects.create(
                     user_id           = user_id,
                     order_number      = user_id + "-" + datetime.now().strftime('%Y-%m-%d-%H-%M-%S-%f'),
                     status_id         = 1,
                     address_id        = Address.objects.get(user_id=user_id, is_active=1).id,
                     payment_method_id = 1
                 )
-                order_cart_id = order_cart.id
+                new_order_id = new_order.id
             else:
-                order_cart_id = Order.objects.get(user_id=user_id, status_id=1).id
+                new_order_id = Order.objects.get(user_id=user_id, status_id=1).id
 
             # 사용자의 장바구니에 이미 해당 상품이 있는 경우 전달된 quantity 만큼 그 수량을 올림
-            if Cart.objects.filter(order_id=order_cart_id, product_id=data['product_id']).exists():
-                product_in_cart = Cart.objects.get(order_id=order_cart_id, product_id=data['product_id'])
+            if Cart.objects.filter(order_id=new_order_id, product_id=data['product_id']).exists():
+                product_in_cart = Cart.objects.get(order_id=new_order_id, product_id=data['product_id'])
                 product_in_cart.quantity += int(data['quantity'])
                 product_in_cart.save()
                 return JsonResponse({'MESSAGE': 'SUCCESS'}, status=200)
             else:
                 Cart.objects.create(
-                    order_id   = order_cart_id,
+                    order_id   = new_order_id,
                     product_id = data['product_id'],
                     quantity   = data['quantity'],
                 )
@@ -90,8 +92,8 @@ class CartView(View):
     # @login_required
     def patch(self, request):
         try:
-            data = json.loads(request.body)
-            delta = data['delta']
+            data      = json.loads(request.body)
+            delta     = data['delta']
             cart_item = Cart.objects.get(id=data['cart_item_id'])
 
             if delta == "minus":
@@ -101,7 +103,7 @@ class CartView(View):
             elif delta == "plus":
                 cart_item.quantity += 1
             else:
-                return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+                return JsonResponse({"MESSAGE": "KEY_ERROR => 'delta' SHOULD BE 'minus' OR 'plus'"}, status=400)
             cart_item.save()
 
             return JsonResponse({'MESSAGE': 'SUCCESS'}, status=201)
