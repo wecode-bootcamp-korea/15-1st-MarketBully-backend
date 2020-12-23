98e7d5836f6fb4f38d27f0bd086e600c40bec0d8
supersaloon <supersaloon@gmail.com>
2020-12-23 17:07:40 +0900

    board/urls.py 수정

--- board/urls.py
+++ board/urls.py
@@ -4,7 +4,5 @@ from .views  import ReviewView, ReviewListView
 urlpatterns = [
     path('/review', ReviewView.as_view()),
     path('/review/<int:review_id>', ReviewView.as_view()),
-    path('/review/<int:review_id>', ReviewView.as_view()),
-    path('/review/<int:review_id>', ReviewView.as_view()),
     path('/review/product/<int:product_id>', ReviewListView.as_view()),
 ]
