bbfd00b35c62c23d1751aa14b54d8f898deb9058
supersaloon <supersaloon@gmail.com>
2020-12-23 21:49:07 +0900

    Modify: 문의 글 출력시 최신순(-create_at)으로 정렬, 문의 글 상세 조회시 리스트에 담지 않음

--- board/views.py
+++ board/views.py
@@ -28,31 +28,29 @@ class QuestionView(View):
         except ValidationError as e:
             return JsonResponse({"MESSAGE": "KEY_ERROR => " + str(e.args[0])}, status=400)
 
-    @signin_decorator
     def get(self, request, question_id):
         try:
             question = Question.objects.get(id=question_id)
-            question_post = [{
+            question_post = {
                 'id'         : question.id,
                 'product_id' : question.product_id,
                 'title'      : question.title,
                 'contents'   : question.contents,
                 'private'    : question.is_private,
-            }]
+                'create_at'  : question.created_at,
+            }
 
             return JsonResponse({"MESSAGE": "SUCCESS", "question_post": question_post}, status=201)
 
         except Question.DoesNotExist as e:
            return JsonResponse({"MESSAGE": "KEY_ERROR => " + e.args[0]}, status=400)
 
-
     @signin_decorator
     def patch(self, request, question_id):
         try:
             data = json.loads(request.body)
-            user_id = "3"
 
-            question = Question.objects.get(id=question_id, author_id=user_id)
+            question = Question.objects.get(id=question_id, author_id=request.user.id)
             if data.get('title'):
                 question.title = data.get('title')
             if data.get('contents'):
@@ -71,9 +69,7 @@ class QuestionView(View):
     @signin_decorator
     def delete(self, request, question_id):
         try:
-            user_id = "3"
-
-            question = Question.objects.get(id=question_id, author_id=user_id)
+            question = Question.objects.get(id=question_id, author_id=request.user.id)
             question.delete()
 
             return JsonResponse({'MESSAGE': 'SUCCESS'}, status=204)
@@ -83,21 +79,21 @@ class QuestionView(View):
 
 
 class QuestionListView(View):
-    @signin_decorator
     def get(self, request, product_id):
         try:
             offset = int(request.GET.get('offset'), 0)
             limit  = int(request.GET.get('limit'), 10)
             limit += offset
 
-            questions = Question.objects.order_by('-id').filter(product_id=product_id)
+            questions = Question.objects.order_by('-create_at').filter(product_id=product_id)
 
             question_list = [{
-                'id'         : question.id,
-                'product_id' : question.product_id,
-                'title'      : question.title,
-                'contents'   : question.contents,
-                'private'    : question.is_private,
+                'id'        : question.id,
+                'product_id': question.product_id,
+                'title'     : question.title,
+                'contents'  : question.contents,
+                'private'   : question.is_private,
+                'create_at' : question.created_at,
 
             }for question in questions[offset:limit]]
 
