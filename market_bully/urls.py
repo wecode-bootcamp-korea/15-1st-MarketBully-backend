49190beff325c58ca16ca724554357ab1722641c
supersaloon <supersaloon@gmail.com>
2020-12-23 11:06:02 +0900

    Modify: conflic 수정 - market_bully/urls.py

--- market_bully/urls.py
+++ market_bully/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('product', include('product.urls')),
+    path('user', include('user.urls')),
     path('order', include('order.urls')),
+    path('product', include('product.urls')),
 ]
