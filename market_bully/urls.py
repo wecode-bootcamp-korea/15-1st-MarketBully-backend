12a5ebd994a49812bebf545a480fc5f56094c2b9
Jacob <jacob.juhyung.lee@gmail.com>
2020-12-23 10:32:33 +0900

    FIX: DS.Store 컨플릭트 해결 - .DS.Store를 .gitignore에 추가해주었습니다

--- market_bully/urls.py
+++ market_bully/urls.py
@@ -20,8 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('product', include('product.urls')),
     path('user', include('user.urls')),
-<<<<<<< HEAD
     ]
-=======
-    ]
->>>>>>> master
