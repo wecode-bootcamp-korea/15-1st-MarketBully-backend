760e83686bc7df7a6b1759fc0de3fe9ba8260d4a
Hyeonju <hjulee12@gmail.com>
2020-12-23 19:54:13 +0900

    frontend와 내용 맞춰보면서 address값 수정

--- user/urls.py
+++ user/urls.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from django.urls import path
-from .views import SignupView, SigninView
+from .views import SignupView, SigninView, FindAccountView
 
 urlpatterns = [
     path('/signup', SignupView.as_view()),
     path('/signin', SigninView.as_view()),
+    path('/findaccount', FindAccountView.as_view())
 ]
